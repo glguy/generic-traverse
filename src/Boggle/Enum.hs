a0d87ed1669d40c006699c9e04e8ea8dc9df6657
Eric Mertens <emertens@gmail.com>
2015-11-09 23:24:54 -0800

    Make Boggle.Enum a standalone module

--- src/Boggle/Enum.hs
+++ src/Boggle/Enum.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -8,7 +9,7 @@
 -- the list to help deal with recursive types.
 --
 -- The key insight here is that by reassociating all '>>=' to the right
--- using 'Codensity' that all of the generics constructors are collected
+-- using 'BindK' that all of the generics constructors are collected
 -- together. This makes it possible for GHC to optimize away the generic
 -- representation.
 module Boggle.Enum
@@ -19,22 +20,20 @@ module Boggle.Enum
   , demos
   ) where
 
+import Control.Applicative      (liftA)
+import Data.Void                (Void)
 import GHC.Generics
-import Data.Void                        (Void)
-import Control.Applicative              (empty)
-import Control.Monad.Codensity          (Codensity(..), lowerCodensity)
-import Control.Monad.Logic.Class        ((>>-), interleave)
 
 -- | This class provides a list of all the elements of a type.
 class Enumerate a where
   enumerate :: [a]
 
   default enumerate :: (Generic a, GEnumerate (Rep a)) => [a]
-  enumerate = lowerCodensity (to <$> genumerate)
+  enumerate = lowerBindK (to <$> genumerate)
 
 
 class GEnumerate g where
-  genumerate :: Codensity [] (g a)
+  genumerate :: BindK [] (g a)
 
 instance GEnumerate f => GEnumerate (M1 i c f) where
   genumerate = M1 <$> genumerate
@@ -46,7 +45,7 @@ instance (GEnumerate f, GEnumerate g) => GEnumerate (f :*: g) where
   genumerate = (:*:) <$> genumerate <*> genumerate
 
 instance Enumerate b => GEnumerate (K1 i b) where
-  genumerate = K1 <$> Codensity (enumerate >>-)
+  genumerate = K1 <$> BindK (enumerate >>-)
 
 instance GEnumerate U1 where
   genumerate = pure U1
@@ -54,8 +53,48 @@ instance GEnumerate U1 where
 instance GEnumerate V1 where
   genumerate = empty
 
-(|||) :: Codensity [] a -> Codensity [] a -> Codensity [] a
-Codensity m ||| Codensity n = Codensity (\k -> interleave (m k) (n k))
+------------------------------------------------------------------------
+
+-- | Local implementation of @Codensity@ type from @kan-extensions@.
+-- This type captures the concept of a partially applied '>>=' function.
+--
+-- While this type support many other instances we focus on the ones
+-- needed to implement this example.
+newtype BindK f a = BindK (forall b. (a -> f b) -> f b)
+
+instance Functor (BindK f) where
+  fmap = liftA
+
+instance Applicative (BindK f) where
+  pure x              = BindK $ \k  -> k x
+  BindK m <*> BindK n = BindK $ \k  ->
+                            m $ \ab ->
+                            n $ \a  ->
+                            k $ ab a
+
+-- | Run a @'BindK' f@ computation with 'pure' as the final continuation.
+lowerBindK :: Applicative f => BindK f a -> f a
+lowerBindK (BindK k) = k pure
+
+empty :: BindK [] a
+empty = BindK (\_ -> [])
+
+------------------------------------------------------------------------
+
+-- | Combine two lists together alternating alternating from each and
+-- starting with the first list.
+interleave :: [a] -> [a] -> [a]
+interleave (x:xs) ys = x : interleave ys xs
+interleave []     ys = ys
+
+-- | "Fair" bind operation for lists.
+(>>-) :: [a] -> (a -> [b]) -> [b]
+[]     >>- _ = []
+(x:xs) >>- f = interleave (f x) (xs >>- f)
+
+-- | 'interleave' operation lifted to 'BindK'
+(|||) :: BindK [] a -> BindK [] a -> BindK [] a
+BindK m ||| BindK n = BindK (\k -> interleave (m k) (n k))
 
 infixl 3 |||
 
